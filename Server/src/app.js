4b2e2a84b7459fdf1a6fcc451e32dcab4e2d2b9c
TWalt03 <tylerwalters537@gmail.com>
2026-04-01 21:58:53 +0200

    Updating http server from local host to dev

--- Server/src/app.js
+++ Server/src/app.js
@@ -28,8 +28,8 @@ app.use(express.json());
 
 // Connect to DB FIRST, then start server
 connectDB().then(() => {
-  httpServer.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  httpServer.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
   });
 });
 
